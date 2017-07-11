70e439156fa25960ce6d9d9a003da5f15e0b57bd
carono <info@carono.ru>
2017-07-11 13:21:17 +0500

    init UrlBehaviours

---
+++ UrlBehaviors.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace carono\yii2behaviors;
+
+use yii\base\Behavior;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
+
+class UrlBehaviors extends Behavior
+{
+    protected $_urlRules = [];
+
+    public $rules = [];
+
+    protected function getUrlRules()
+    {
+        return is_string($this->rules) ? call_user_func([$this->owner, $this->rules]) : $this->rules;
+    }
+
+    /**
+     * @return UrlRule[]
+     */
+    protected function normalizeUrlRules()
+    {
+        /**
+         * @var $urlRule UrlRule
+         */
+        if ($this->_urlRules) {
+            return $this->_urlRules;
+        }
+        foreach ($this->getUrlRules() as $rule) {
+            $action = ArrayHelper::remove($rule, 0);
+            $rule['action'] = $action;
+            $rule['class'] = UrlRule::className();
+            $rule['model'] = $this->owner;
+            $urlRule = \Yii::createObject($rule);
+            $this->_urlRules[] = $urlRule;
+        }
+        return $this->_urlRules;
+    }
+
+    /**
+     * @param $action
+     * @param bool $asString
+     * @return array|mixed|string
+     */
+    public function getUrl($action, $asString = false)
+    {
+        $url = [];
+        foreach ($this->normalizeUrlRules() as $rule) {
+            if ($rule->compare($action, \Yii::$app->user->getIdentity())) {
+                $url = $rule->url;
+                break;
+            }
+        }
+        return $asString ? Url::to($url, true) : $url;
+    }
+}
---
+++ UrlRule.php
@@ -0,0 +1,99 @@
+<?php
+
+
+namespace carono\yii2behaviors;
+
+
+use carono\yii2rbac\CurrentUser;
+use carono\yii2rbac\RoleManager;
+use yii\base\Component;
+use yii\db\ActiveRecord;
+
+/**
+ * Class UrlRule
+ *
+ * @property mixed action
+ * @property mixed url
+ * @package app\components
+ */
+class UrlRule extends Component
+{
+    protected $_original_action;
+    protected $_action;
+    protected $_modifier;
+    protected $_url;
+    public $role;
+    public $application;
+    public $params = [];
+    /**
+     * @var ActiveRecord
+     */
+    public $model;
+
+    protected static function haveRole($role, $user)
+    {
+        return in_array($role, self::getRoles($user));
+    }
+
+    protected static function getRoles($user)
+    {
+        return array_keys(\Yii::$app->authManager->getRolesByUser($user->id));
+    }
+
+    public function compare($action, $user)
+    {
+        $needUser = \Yii::$app->user->getIdentity()->getId() && $user->id;
+        $needApplication = \Yii::$app->id == $this->application || !$this->application;
+        $needRole = false;
+        $needAction = $action == $this->_original_action;
+        if ($this->role) {
+            foreach ($this->role as $role) {
+                if (self::haveRole($role, $user) || in_array($role, self::getRoles($user))) {
+                    $needRole = true;
+                    break;
+                }
+            }
+        } else {
+            $needRole = true;
+        }
+        return $needAction && $needUser && $needApplication && $needRole;
+    }
+
+    public function setUrl($value)
+    {
+        $this->_url = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        $url = $this->_url;
+        if (is_string($url)) {
+            $url = [$url];
+        }
+        foreach ($this->params as $key => $param) {
+            $url[is_numeric($key) ? $param : $key] = $this->model->getAttribute($param);
+        }
+        return $url;
+    }
+
+    public function setAction($action)
+    {
+        $this->_original_action = $action;
+        $arr = explode('/', $action);
+        $this->_action = $arr[0];
+        $this->_modifier = count($arr) == 2 ? $arr[1] : null;
+    }
+
+    public function getAction()
+    {
+        return $this->_action;
+    }
+
+    public function getModifier()
+    {
+        return $this->_modifier;
+    }
+}
