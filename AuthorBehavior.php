50bde2621c07ac8cd9553a5fc0a091f6c9af84c4
carono <info@carono.ru>
2017-10-10 15:26:52 +0500

    fix AuthorBehavior

--- AuthorBehavior.php
+++ AuthorBehavior.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace carono\yii2behaviors;
 
 use yii\behaviors\AttributeBehavior;
@@ -7,38 +8,42 @@ use yii\helpers\ArrayHelper;
 
 class AuthorBehavior extends AttributeBehavior
 {
-	public $attributes;
-	public $createdByAttribute = 'creator_id';
-	public $updatedByAttribute = 'updater_id';
+    public $attributes;
+    public $createdByAttribute = 'creator_id';
+    public $updatedByAttribute = 'updater_id';
 
-	public function init()
-	{
-		parent::init();
+    public function init()
+    {
+        parent::init();
 
-		if (empty($this->attributes)) {
-			$this->attributes = [
-				BaseActiveRecord::EVENT_BEFORE_INSERT => [$this->createdByAttribute, $this->updatedByAttribute],
-				BaseActiveRecord::EVENT_BEFORE_UPDATE => $this->updatedByAttribute,
-			];
-		}
-	}
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                BaseActiveRecord::EVENT_BEFORE_INSERT => [$this->createdByAttribute, $this->updatedByAttribute],
+                BaseActiveRecord::EVENT_BEFORE_UPDATE => $this->updatedByAttribute,
+            ];
+        }
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	protected function getValue($event)
-	{
-		if ($event->name == BaseActiveRecord::EVENT_BEFORE_INSERT) {
-			$attr = ArrayHelper::getValue(
-				$this->attributes, BaseActiveRecord::EVENT_BEFORE_INSERT, $this->createdByAttribute
-			);
-			if (is_array($attr)) {
-				$attr = current($attr);
-			}
-			if ($value = $event->sender->{$attr}) {
-				return $value;
-			}
-		}
-		return \Yii::$app->user->getId();
-	}
+    /**
+     * @inheritdoc
+     */
+    protected function getValue($event)
+    {
+        if ($event->name == BaseActiveRecord::EVENT_BEFORE_INSERT) {
+            $attr = ArrayHelper::getValue(
+                $this->attributes, BaseActiveRecord::EVENT_BEFORE_INSERT, $this->createdByAttribute
+            );
+            if (is_array($attr)) {
+                $attr = current($attr);
+            }
+            if ($value = $event->sender->{$attr}) {
+                return $value;
+            }
+        }
+        if (isset(\Yii::$app->user)) {
+            return \Yii::$app->user->getId();
+        } else {
+            return null;
+        }
+    }
 }
